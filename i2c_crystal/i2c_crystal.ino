254eb0b1e55df732f2055f16b59d4eac18d884a5
2021320meas <81633528+2021320meas@users.noreply.github.com>
2022-03-19 07:21:26 +0700

    Update i2c_crystal.ino

--- i2c_crystal/i2c_crystal.ino
+++ i2c_crystal/i2c_crystal.ino
@@ -1,32 +1,29 @@
 #include <LiquidCrystal_I2C.h>
 #include <Keypad.h>
-#include <SoftwareSerial.h>
 #include <Servo.h>
 #include <NewPing.h>
-#define trig 2
-#define echo 3
+#define trig 4
+#define echo 2
 NewPing sothyro(trig, echo, 100);
 Servo servo;
-int servoPin = 13;
+int servoPin = 3;
 int angle = 0;
 int plastic = 0;
 int cans = 0;
 const byte ROWS = 4; //four rows
 const byte COLS = 4; //four columns
 String number = "";
+int calculation = 0;
 char keys[ROWS][COLS] = {
-  {'1', '2', '+', '3'},
-  {'4', '5', 'B', '6'},
-  {'7', '8', 'C', '9'},
-  {'*', '0', 'D', '#'}
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'}
 };
-SoftwareSerial mySerial(12, 11);
-byte rowPins[ROWS] = { 4, 5, 6, 7}; //connect to the row pinouts of the keypad
-byte colPins[COLS] = {8, 9, 10, 11}; //connect to the column pinouts of the keypad
+byte rowPins[ROWS] = { 6, 7, 8, 9}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {10, 11, 12, 13}; //connect to the column pinouts of the keypad
 Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
 LiquidCrystal_I2C lcd(0x27, 20, 4);
-
-
 void setup()
 {
   Serial.begin(9600);
@@ -34,54 +31,52 @@ void setup()
   lcd.begin();
   lcd.clear();
   lcd.backlight();
-  mySerial.begin(9600);
 }
-
-
 void loop() {
-
-
   int cool = sothyro.ping_cm();
   char key = keypad.getKey();
   Serial.print(key);
   Serial.print(cool);
   lcd.setCursor(0, 2);
-  lcd.print("PH number input :");
+  lcd.print("Amount: ");
+  lcd.print(calculation);
+  lcd.print(" riels");
   lcd.setCursor(0, 0);
   lcd.print("Plastic :");
   lcd.print(plastic);
   lcd.setCursor(0, 1);
   lcd.print("Cans : ");
   lcd.print(cans);
-  delay(1000);
   if (cool <= 3 && cool >= 1) {
+    delay(1000);
     for (angle = 90; angle <= 180; angle += 30) {
       servo.write(angle);
     }
     delay(1500);
     for (angle = 180; angle >= 90; angle -= 30) {
       servo.write(angle);
-    }
+    }   
     plastic += 1;
+    calculation += 10;
   }
-  else if (cool >= 11 && cool <= 13) {
-    for (angle = 90; angle >= 0; angle -= 30) {
+  else if (cool <= 8 && cool >= 7) {
+    delay(1000);
+    for (angle = 90; angle <= 180; angle += 30) {
       servo.write(angle);
     }
     delay(1500);
-    for (angle = 0; angle <= 90; angle += 30) {
+    for (angle = 180; angle >= 90; angle -= 30) {
       servo.write(angle);
-    }
+    }   
     cans += 1;
+    calculation += 50;
   }
-
-
-  if (key == '#') // we check if someone pressed #
+    if (key == '#') // we check if someone pressed #
   {
     // enter the phone number
     number = Take_input();
+
   }
-  delay(500);
 }
 
 String Take_input (void)
@@ -109,27 +104,16 @@ String Take_input (void)
     }
   }
   lcd.clear();
+  lcd.setCursor(1,5);
   lcd.print("Money Sent");
-  plastic =0;
+  lcd.clear();
+  plastic = 0;
   cans = 0;
-  lcd.setCursor(0,2);
-  lcd.print("PH number input : ");
+  calculation =0;
   return num;
 }
 void print_content (String str)
 {
   lcd.setCursor(0, 3);
   lcd.print(str);
-}
-void updateSerial()
-{
-  delay(500);
-  while (Serial.available())
-  {
-    mySerial.write(Serial.read());//Forward what Serial received to Software Serial Port
-  }
-  while (mySerial.available())
-  {
-    Serial.write(mySerial.read());//Forward what Software Serial received to Serial Port
-  }
 }
